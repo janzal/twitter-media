7ce0cc313316b1aa9a515444909f1ed1fbcc5b9d
Jan Zaloudek <mail@janzaloudek.cz>
2015-06-16 11:14:49 +0200

    uploading larger video in example

--- examples/video.js
+++ examples/video.js
@@ -2,7 +2,7 @@ var MediaUpload = require('../');
 var fs = require('fs');
 var oauthCreds = require('../config0.js');
 
-var media = fs.readFileSync('./video.mp4');
+var media = fs.readFileSync('./video2.mp4');
 
 var media_upload = new MediaUpload(oauthCreds);
 media_upload.uploadMedia('video', media, console.log);
