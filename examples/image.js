93882fd20a9aeb831547252b7a199da75da3ac79
Cameron Hunter <chunter@netflix.com>
2017-07-11 22:40:39 -0700

    Fix examples so that current working directory can be anywhere

--- examples/image.js
+++ examples/image.js
@@ -1,8 +1,9 @@
 var MediaUpload = require('../');
 var fs = require('fs');
 var oauthCreds = require('../config0.js');
+var path = require('path');
 
-var media = fs.readFileSync('./image.jpg');
+var media = fs.readFileSync(path.join(__dirname, 'image.jpg'));
 
 var media_upload = new MediaUpload(oauthCreds);
 media_upload.uploadMedia('image', media, console.log);
